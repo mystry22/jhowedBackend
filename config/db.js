bde9c9e3d0749248b88e008a77d5cca9f6e263a8
mystry22 <alanemehenry6@gmail.com>
2022-10-30 13:51:59 -0700

    edited db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const testDb = 'mongodb://127.0.0.1/jhowed';
-const dbUrl = 'mongodb+srv://mystry:mystry22@fancyfinery.k3uod.mongodb.net/efex?retryWrites=true&w=majority';
-mongoose.connect(testDb, {useUnifiedTopology:true, useNewUrlParser:true},()=>{
+const dbUrl = 'mongodb+srv://mystry:mystry22@fancyfinery.k3uod.mongodb.net/jhowed?retryWrites=true&w=majority';
+mongoose.connect(dbUrl, {useUnifiedTopology:true, useNewUrlParser:true},()=>{
     console.log('db connection ok for jhowed');
 });
